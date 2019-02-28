21c42e22a9d36b699c21731804ba1d99beeade5e
luck <likangios@163.com>
2019-02-28 16:47:53 +0800

    no message

--- SMXCAlbum/Module/TiaokuanViewController.m
+++ SMXCAlbum/Module/TiaokuanViewController.m
@@ -10,7 +10,7 @@
 #import <WebKit/WebKit.h>
 #import "AppDelegate.h"
 #import <AFNetworking.h>
-@interface TiaokuanViewController ()
+@interface TiaokuanViewController ()<UINavigationBarDelegate,UIScrollViewDelegate,WKNavigationDelegate>
 
 @property(nonatomic,strong) WKWebView *webView;
 
@@ -36,6 +36,7 @@
         _webView = [[WKWebView alloc] init];
         _webView.allowsBackForwardNavigationGestures = YES;
         _webView.allowsLinkPreview = false;
+        _webView.navigationDelegate = self;
     }
     return _webView;
 }
@@ -54,7 +55,8 @@
         make.bottom.mas_equalTo(-40);
         make.size.mas_equalTo(CGSizeMake(200, 40));
     }];
-    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://jiakao.xianwan.com/driver/index/policy?id=4"]]];
+    NSString *url = @"https://www.jianshu.com/p/63a57cdf47c1";
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
     [[self.confirmButton rac_signalForControlEvents:UIControlEventTouchUpInside] subscribeNext:^(__kindof UIControl * _Nullable x) {
         [self dismissViewControllerAnimated:YES completion:NULL];
     }];
@@ -63,17 +65,17 @@
     [manager setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
         switch (status) {
             case AFNetworkReachabilityStatusUnknown:
-                
+
                 break;
             case AFNetworkReachabilityStatusNotReachable:
-                
+
                 break;
             case AFNetworkReachabilityStatusReachableViaWiFi:
                 [self.
-                 webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://jiakao.xianwan.com/driver/index/policy?id=4"]]];
+                 webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
                 break;
             case AFNetworkReachabilityStatusReachableViaWWAN:
-                [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:@"http://jiakao.xianwan.com/driver/index/policy?id=4"]]];
+                [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL URLWithString:url]]];
                 break;
             default:
                 break;
@@ -81,6 +83,34 @@
     }];
     [manager startMonitoring];
 }
+- (void)webView:(WKWebView *)webView didFinishNavigation:(null_unspecified WKNavigation *)navigation{
+    //顶部 下载APP
+    [webView evaluateJavaScript:@"document.getElementsByClassName('header-wrap')[0].style.display = 'none'" completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+    }];
+    //底部打开APP
+    [webView evaluateJavaScript:@"document.getElementsByClassName('footer-wrap')[0].style.display = 'none'" completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+        
+    }];
+    //底部 登录 打开 热门
+    [webView evaluateJavaScript:@"document.getElementsByClassName('panel')[0].style.display = 'none'" completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+        
+    }];
+    //顶部打卡APP
+    [webView evaluateJavaScript:@"document.getElementsByClassName('app-open')[0].style.display = 'none'" completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+        
+    }];
+    //中部 打开APP阅读
+    [webView evaluateJavaScript:@"document.getElementsByClassName('open-app-btn')[0].style.display = 'none'" completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+        
+    }];
+    //个人信息
+    [webView evaluateJavaScript:@"document.getElementsByClassName('article-info')[0].style.display = 'none'" completionHandler:^(id _Nullable obj, NSError * _Nullable error) {
+        
+    }];
+    //    [SVProgressHUD dismiss];
+    
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
